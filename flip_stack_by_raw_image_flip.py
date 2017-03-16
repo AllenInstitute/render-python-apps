fdc67979b267d5bb4bda6705b9cff7589fb3a8cf
Forrest Collman <forrestc@alleninstitute.org>
2017-03-16 15:29:36 -0700

    fixed bugs

--- flip_stack_by_raw_image_flip.py
+++ flip_stack_by_raw_image_flip.py
@@ -19,8 +19,8 @@ example_parameters = {
         "project":"M247514_Rorb_1",
         "client_scripts":"/pipeline/render/render-ws-java-client/src/main/scripts"
     },
-    'input_stack':'EM_Site4_stitched',
-    'output_stack':'EM_Site4_stitched',
+    'input_stack':'ACQDAPI_1',
+    'output_stack':'ACQDAPI_1_junkflip3',
     'minZ':0,
     'maxZ':0,
     'delete_after':False
@@ -29,8 +29,8 @@ example_parameters = {
 class FlipStackParameters(RenderParameters):
     input_stack = mm.fields.Str(required=True,metadata={'description':'stack to apply affine to'})
     output_stack = mm.fields.Str(required=False,metadata={'description':'stack to save answer into (defaults to overwriting input_stack)'})
-    minZ = mm.fields.Int(required=False,metadata={'description':'minimum Z to flip'})
-    maxZ = mm.fields.Int(required=False,metadata={'description':'maximum Z to flip'})
+    minZ = mm.fields.Int(required=True,metadata={'description':'minimum Z to flip'})
+    maxZ = mm.fields.Int(required=True,metadata={'description':'maximum Z to flip'})
     pool_size = mm.fields.Int(required=False,default=20,metadata={'description':'size of pool for parallel processing (default=20)'})
     delete_after = mm.fields.Boolean(required=False,default=False,metadata={'description':'whether to delete the old image files or not after flipping'})
 
@@ -39,8 +39,13 @@ if __name__ == '__main__':
     mod = RenderModule(schema_type=FlipStackParameters,input_data=example_parameters)
     #mod.run()
     #get the z values in the stack
-    zvalues = mod.render.run(renderapi.stack.get_z_values_for_stack,mod.args['input_stack'])
+    zvalues = range(mod.args['minZ'],mod.args['maxZ']+1)
 
+
+    def fix_url(url):
+        path = url.replace('file:','')
+        path = path.replace('%20',' ')
+        return path
     #define a function to process one z value
     def process_z(render,input_stack,z,delete_after=False):
         #get the tilespecs for this Z
@@ -48,15 +53,19 @@ if __name__ == '__main__':
         #loop over the tilespes adding the transform
         for ts in tilespecs:
             #slide this tile up according to its minimumX
-            slide_up = AffineModel(B1=-ts.minX)
+            slide_up = AffineModel(B1=-2*ts.minY)
             ts.tforms.append(slide_up)
 
             #get the old minimum mipmaplevel
             mml = ts.ip.mipMapLevels[0]
             old_url = mml.imageUrl
-            new_url = os.path.splitext(mml.imageUrl)[1]+'_flip.tif'
+            old_path = fix_url(old_url)
+
+            new_url = mml.imageUrl[0:-4]+'_flip.tif'
+            new_path = fix_url(new_url)
+            
             #construct the imagemagick command
-            cmd = ['convert',mml.imageUrl,'_flip',new_url]
+            cmd = ['convert',old_path,'-flip',new_path]
             mml.imageUrl = new_url
             ts.ip.update(mml)
             
@@ -84,10 +93,15 @@ if __name__ == '__main__':
     #define a processing pool
     pool = Pool(mod.args['pool_size'])
     #define a partial function for processing a single z value
-    mypartial = partial(process_z,mod.render,mod.args['input_stack'],global_tform,delete_after=mod.args['delete_after'])
+    mypartial = partial(process_z,mod.render,mod.args['input_stack'],delete_after=mod.args['delete_after'])
 
-    #get the filepaths of json files in parallel
+    #mypartial(0)
+    #get the filepaths of json files in parallel   
     json_files = pool.map(mypartial,zvalues)
+
+    if mod.args['input_stack']!=output_stack:
+        renderapi.stack.create_stack(output_stack,render=mod.render)
+
     #import the json_files into the output stack
     renderapi.client.import_jsonfiles_parallel(output_stack,json_files,poolsize=mod.args['pool_size'],render=mod.render)
 
