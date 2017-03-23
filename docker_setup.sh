9cbaccaf2ce40c0c4609302d94df38d9d479023c
Forrest Collman <forrestc@alleninstitute.org>
2017-03-22 17:06:21 -0700

    adding new docker_setup script

--- docker_setup.sh
+++ docker_setup.sh
@@ -1,6 +1,8 @@
-docker build -t renderapps:latest .
-docker tag renderapps:latest fcollman/render-python-apps
-docker tag renderapps:latest atbigdawg:5000/fcollman/render-python-apps
+docker pull atbigdawg:5000/fcollman/render-python
+docker tag atbigdawg:5000/fcollman/render-python fcollman/render-python
+docker build -t fcollman/render-python-apps .
+docker tag fcollman/render-python-apps atbigdawg:5000/fcollman/render-python-apps
+docker push atbigdawg:5000/fcollman/render-python-apps
 docker kill renderapps
 docker rm renderapps
 docker run -d --name renderapps \
@@ -14,7 +16,7 @@ docker run -d --name renderapps \
 -v /etc/hosts:/etc/hosts \
 -p 8888:8888 \
 -e "PASSWORD=$JUPYTERPASSWORD" \
--i -t renderapps:latest \
+-i -t fcollman/render-python-apps \
 /bin/bash -c "/opt/conda/bin/jupyter notebook --config=/root/.jupyter/jupyter_notebook_config.py --notebook-dir=/pipeline/render-python-apps --no-browser" 
  
 
