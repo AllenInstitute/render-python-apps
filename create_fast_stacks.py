3227f9b1fee34686af7b38b691930955e65e3694
Forrest Collman <forrestc@alleninstitute.org>
2017-03-24 13:46:01 -0700

    made better module

--- create_fast_stacks.py
+++ create_fast_stacks.py
@@ -7,13 +7,25 @@ import copy
 import renderapi
 from renderapi.tilespec import TileSpec,Layout,MipMapLevel
 from renderapi.transform import AffineModel
-
+from create_mipmaps import create_mipmaps
 my_env = os.environ.copy()
 from itertools import izip_longest
+from pathos.multiprocessing import Pool
+from render_module import RenderModule,RenderParameters
+from json_module import InputFile,InputDir
+import marshmallow as mm
 
+class CreateFastStacksParameters(RenderParameters):
+    statetableFile = InputFile(required=True,
+        metadata={'description':'state table file'})
+    projectDirectory = InputDir(required=True,
+        metadata={'description':'path to project root'})
+    outputStackPrefix = mm.fields.Str(required=False,default="ACQ",
+        metadata={'description':'prefix to include in front of channel name for render stack'})
+    pool_size = mm.fields.Int(require=False,default=20,
+        metadata={'description':'number of parallel threads to use'})
 
-
-def make_tilespec_from_statetable (df,rootdir,outputProject,outputOwner,minval=0,maxval=50000):
+def make_tilespec_from_statetable (df,rootdir,outputProject,outputOwner,outputStack,minval=0,maxval=50000):
     df = df[df['zstack']==0]
     #ribbons = df.groupby('ribbon')
     #zoffset=0
@@ -24,11 +36,11 @@ def make_tilespec_from_statetable (df,rootdir,outputProject,outputOwner,minval=0
     #    df.loc[ribbon.index,'z']=ribbon['z'].values
 
 
-    cmds = []
+    mipmap_args = []
     tilespecpaths = []
     for ((ch,sess),chgroup) in df.groupby(['ch_name','session']):
         print ch,sess
-    
+
         for ((rib,sect),group) in chgroup.groupby(['ribbon','section']):
             tilespeclist=[]
             z=0
@@ -44,10 +56,11 @@ def make_tilespec_from_statetable (df,rootdir,outputProject,outputOwner,minval=0
 
                 if not os.path.exists(downdir):
                     os.makedirs(downdir)
-               
-                        #construct command for creating mipmaps for this tilespec
-                downcmd = ['python','create_mipmaps.py','--inputImage',filepath,'--outputDirectory',downdir,'--mipmaplevels','1','2','3']
-                cmds.append(downcmd)
+
+                #construct command for creating mipmaps for this tilespec
+                #downcmd = ['python','create_mipmaps.py','--inputImage',filepath,'--outputDirectory',downdir,'--mipmaplevels','1','2','3']
+                #cmds.append(downcmd)
+                mipmap_args.append((filepath,downdir))
                 layout = Layout(sectionId=row.ribbon*1000+row.section,
                                                 scopeId='Leica',
                                                 cameraId='zyla',
@@ -57,7 +70,7 @@ def make_tilespec_from_statetable (df,rootdir,outputProject,outputOwner,minval=0
                                                 stageY = row.ystage,
                                                 rotation = 0.0,
                                                 pixelsize = row.scale_x)
-                
+
                 mipmap0 = MipMapLevel(level=0,imageUrl=row.full_path)
                 mipmaplevels=[mipmap0]
                 filename = "%s_S%04d_F%04d_Z%02d.tif"%(row.ch_name,row.section,row.frame,0)
@@ -72,7 +85,7 @@ def make_tilespec_from_statetable (df,rootdir,outputProject,outputOwner,minval=0
                                          M11=row.a11,
                                          B0=row.a02,
                                          B1=row.a12)
-            
+
                 tilespeclist.append(TileSpec(tileId=row.tileID,
                                      frameId = row.frame,
                                      z=row.z,
@@ -84,66 +97,63 @@ def make_tilespec_from_statetable (df,rootdir,outputProject,outputOwner,minval=0
                                      maxint=maxval,
                                      layout= layout))
                 z = row.z
-        
+
             json_text=json.dumps([t.to_dict() for t in tilespeclist],indent=4)
             json_file = os.path.join(tilespecdir,outputProject+'_'+outputOwner+'_'+outputStack+'_%04d.json'%z)
             fd=open(json_file, "w")
             fd.write(json_text)
             fd.close()
             tilespecpaths.append(json_file)
-    return tilespecpaths,cmds
+    return tilespecpaths,mipmap_args
 
+def create_mipmap_from_tuple(mipmap_tuple):
+    (filepath,downdir)=mipmap_tuple
+    return create_mipmaps(filepath,downdir)
 
+class CreateFastStack(RenderModule):
+    def __init__(self,schema_type=None,*args,**kwargs):
+        if schema_type is None:
+            schema_type = CreateFastStacksParameters
 
+        super(CreateFastStack,self).__init__(schema_type=schema_type,*args,**kwargs)
+    def run(self):
+        outputProject=self.args['render']['project']
+        outputOwner = self.args['render']['owner']
+        statetablefile = self.args['statetableFile']
+        rootdir = self.args['projectDirectory']
 
+        df = pd.read_csv(statetablefile)
+        ribbons = df.groupby('ribbon')
+        k=0
+        pool = Pool(self.args['pool_size'])
+        for ribnum,ribbon in ribbons:
+            mydf = ribbon.groupby('ch_name')
+            for channum,chan in mydf:
+                outputStack = self.args['outputStackPrefix'] + '_%s'%(channum)
 
+                self.logger.info("creating tilespecs and cmds....")
+                tilespecpaths,mipmap_args = make_tilespec_from_statetable(chan,rootdir,outputProject,outputOwner,outputStack)
+                self.logger.info("importing tilespecs into render....")
+                self.logger.info("creating downsampled images ...")
+
+                results=pool.map(create_mipmap_from_tuple,mipmap_args)
+
+                #groups = [(subprocess.Popen(cmd,\
+                # stdout=subprocess.PIPE) for cmd in cmds)] \
+                # * self.args['pool_size'] # itertools' grouper recipe
+                #for processes in izip_longest(*groups): # run len(processes) == limit at a time
+                #   for p in filter(None, processes):
+                #        p.wait()
+                self.logger.info("uploading to render ...")
+                if k==0:
+                    renderapi.stack.delete_stack(outputStack,owner=outputOwner,
+                    project=outputProject,render=self.render)
+                    renderapi.stack.create_stack(outputStack,owner=outputOwner,
+                    project=outputProject,verbose=False,render=self.render)
+                self.logger.info(tilespecpaths)
+                renderapi.client.import_jsonfiles_parallel(outputStack,tilespecpaths,render=self.render)
+            k+=1
 
 if __name__ == "__main__":
-
-
-    DEFAULT_HOST = "ibs-forrestc-ux1"
-    DEFAULT_PORT = 8080
-    DEFAULT_OWNER = "Forrest"
-    DEFAULT_PROJECT = "TEST"
-    DEFAULT_CLIENT_SCRIPTS = '/pipeline/render/render-ws-java-client/src/main/scripts'
-    parser = argparse.ArgumentParser(description="Create fast Stacks")
-    parser.add_argument('--statetableFile', help="State Table File")
-    parser.add_argument('--projectDirectory',        help="name of the input Project")
-    parser.add_argument('--outputStackPrefix', help = "Output Stack Prefix")
-    parser.add_argument('--owner',default=DEFAULT_OWNER,required=False)
-    parser.add_argument('--project',default=DEFAULT_PROJECT,required=False)
-    parser.add_argument('--host',default=DEFAULT_HOST,required=False)
-    parser.add_argument('--port',default=8080,required=False)
-    parser.add_argument('--client_scripts',default=DEFAULT_CLIENT_SCRIPTS,required=False,type=str,help='default client location')
-    args = parser.parse_args()
-
-    outputProject=args.project
-    outputOwner = args.owner
-    statetablefile = args.statetableFile
-    rootdir = args.projectDirectory
-
-    df = pd.read_csv(statetablefile)
-    ribbons = df.groupby('ribbon')  
-    r = renderapi.render.connect(**vars(args))
-    k=0
-    for ribnum,ribbon in ribbons:
-        mydf = ribbon.groupby('ch_name')
-        for channum,chan in mydf:
-            outputStack = args.outputStackPrefix + '_%s'%(channum)
-            
-            print "creating tilespecs and cmds...."
-            tilespecpaths,cmds = make_tilespec_from_statetable (chan,rootdir,outputProject,outputOwner)
-            print "importing tilespecs into render...."
-            print "creating downsampled images ..."
-            groups = [(subprocess.Popen(cmd, stdout=subprocess.PIPE) for cmd in cmds)] * 20 # itertools' grouper recipe
-            for processes in izip_longest(*groups): # run len(processes) == limit at a time
-               for p in filter(None, processes):
-                   p.wait()
-            print "uploading to render ..."
-            if k==0:
-                renderapi.stack.delete_stack(outputStack,owner=outputOwner,project=outputProject,render=r)
-                renderapi.stack.create_stack(outputStack,owner=outputOwner,project=outputProject,verbose=False,render=r)
-            print tilespecpaths
-            renderapi.client.import_jsonfiles_parallel(outputStack,tilespecpaths,render=r)
-        k+=1
-            
+    mod = CreateFastStack(schema_type = CreateFastStacksParameters)
+    mod.run()
