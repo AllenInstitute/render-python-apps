a3ecaf0cfe75623f83b72baf6288c490a379d312
Forrest Collman <forrestc@alleninstitute.org>
2017-03-25 16:33:06 -0700

    minor bug fixes

--- consolidate_render_transforms.py
+++ consolidate_render_transforms.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from renderapi.tilespec import AffineModel
+from renderapi.transform import AffineModel
 import renderapi
 import json
 import os
@@ -9,20 +9,20 @@ import argparse
 from renderapi.utils import stripLogger
 example_json={
     "render":{
-        "host":"ibs-forrestc-ux1",
-        "port":8080,
-        "owner":"Forrest",
+        "host":"http://ibs-forrestc-ux1",
+        "port":80,
+        "owner":"Sharmishtaas",
         "project":"M247514_Rorb_1",
         "client_scripts":"/pipeline/render/render-ws-java-client/src/main/scripts"
     },
-    "stack":"REGFLATSMALLFIXDAPI_1_deconv"
+    "stack":"ROUGHALIGN_MARCH_21_DAPI_1"
 }
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description = "import xml files for each Z to register EM to LM")
     parser.add_argument('--inputJson',help='json based input argument file',type=str)
     parser.add_argument('--verbose','-v',help='verbose logging',action='store_true')
     args = parser.parse_args()
-    jsonargs = json.load(open(args.inputJson,'r'))
+
 
     if args.inputJson is None:
         jsonargs = example_json
@@ -47,7 +47,7 @@ if __name__ == '__main__':
 
     def process_z_make_json(r,json_dir,z,verbose=False):
         def consolidate_transforms(tforms,verbose=False,makePolyDegree=0):
-            
+
             if verbose:
                 points = np.array([[0,0],[1000,1000]])
                 print 'consolidate_transforms:before 0,0 maps to'
@@ -55,16 +55,16 @@ if __name__ == '__main__':
                     points = tform.tform(points)
                     print tform.M
                 print points
-                
+
 
             tform_total = AffineModel()
             start_index = 0
             total_affines = 0
             new_tform_list = []
-          
+
             for i,tform in enumerate(tforms):
                 if 'AffineModel2D' in tform.className:
-                    total_affines+=1    
+                    total_affines+=1
                     tform_total = tform.concatenate(tform_total)
                     #tform_total.M=tform.M.dot(tform_total.M)
                 else:
@@ -87,7 +87,7 @@ if __name__ == '__main__':
                     new_tform_list.append(polyTform)
                 else:
                     new_tform_list.append(tform_total)
-            
+
             if verbose:
                 points = np.array([[0,0],[1000,1000]])
                 print 'consolidate_transforms: after 0,0 maps to '
@@ -96,23 +96,23 @@ if __name__ == '__main__':
                 print points
                 print 'tforms after',new_tform_list
             return new_tform_list
-        
+
         tilespecs = r.run(renderapi.tilespec.get_tile_specs_from_z,stack,z)
-       
-        for ts in tilespecs: 
+
+        for ts in tilespecs:
             if verbose:
                 print ts.tileId
-            ts.tforms = consolidate_transforms(ts.tforms,verbose)   
+            ts.tforms = consolidate_transforms(ts.tforms,verbose)
             if verbose:
                 print 'consolidated tformlist'
                 print ts.tforms[0].M
-                
+
         if verbose:
             print tilespecs[0].tileId,tilespecs[0].tforms
         json_filepath = os.path.join(json_dir,'%s_%04d'%(outstack,z))
         renderapi.utils.renderdump(tilespecs,open(json_filepath,'w'),indent=4)
         return json_filepath
-            
+
     json_files=pool.map(partial(process_z_make_json,r,json_dir),zvalues)
 
     r.run(renderapi.stack.delete_stack,outstack)
