ddd3640e415e6e97d13e28d3855a0966fc128342
Forrest Collman <forrestc@alleninstitute.org>
2017-05-10 16:47:59 -0700

    changed docker run script

--- allen_utils/docker_setup.sh
+++ allen_utils/docker_setup.sh
@@ -5,7 +5,7 @@ docker tag fcollman/render-python-apps atbigdawg:5000/fcollman/render-python-app
 docker push atbigdawg:5000/fcollman/render-python-apps
 docker kill renderapps
 docker rm renderapps
-docker run -d --name renderapps \
+docker run -t --name renderapps \
 -v /nas:/nas \
 -v /nas2:/nas2 \
 -v /nas3:/nas3 \
@@ -14,9 +14,10 @@ docker run -d --name renderapps \
 -v /pipeline:/pipeline \
 -v /pipeline/render-python-apps:/usr/local/render-python-apps \
 -v /etc/hosts:/etc/hosts \
+--dns 10.128.104.10 \
 -p 8888:8888 \
 -e "PASSWORD=$JUPYTERPASSWORD" \
 -i -t fcollman/render-python-apps \
-/bin/bash -c "/opt/conda/bin/jupyter notebook --config=/root/.jupyter/jupyter_notebook_config.py --notebook-dir=/pipeline/render-python-apps --no-browser" 
+/bin/bash -c "/opt/conda/bin/jupyter notebook --config=/root/.jupyter/jupyter_notebook_config.py --notebook-dir=/pipeline/render-python-apps --no-browser --allow-root" 
  
 
