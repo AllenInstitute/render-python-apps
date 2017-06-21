3f3710c9f83e5a5abe7a283577bd8a9239b60dd6
forrest collman <forrest.collman@gmail.com>
2017-06-21 09:42:44 -0700

    added module to subpackage list

--- renderapps/__init__.py
+++ renderapps/__init__.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python
 
-from . import cross_modal_registration
-from . import dataimport
-from . import materialize
-from . import pointmatch
-from . import refactor
-from . import registration
-from . import section_polygons
-from . import stack
-from . import stitching
-from . import tile
-from . import TrakEM2
-from . import transfer
-from . import wrinkle_detection
+#from . import cross_modal_registration
+#from . import dataimport
+#from . import materialize
+#from . import pointmatch
+#from . import refactor
+#from . import registration
+#from . import section_polygons
+#from . import stack
+#from . import stitching
+#from . import tile
+#from . import TrakEM2
+#from . import transfer
+#from . import wrinkle_detection
 
 
 __all__ = ['cross_modal_registration', 'dataimport', 'materialize', 'pointmatch',
+           'module',
            'refactor', 'registration', 'section_polygons', 'stack',
            'stitching', 'tile', 'TrakEM2','transfer','wrinkle_detection']
