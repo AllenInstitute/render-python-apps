02dc269a00b73f3c96f4586fb29c933225a22486
RussTorres <russtorres@gmail.com>
2017-07-19 13:06:58 -0700

    transfer: use argschema

--- renderapps/transfer/transfer_module.py
+++ renderapps/transfer/transfer_module.py
@@ -1,17 +1,16 @@
 from ..module.render_module import RenderClientParameters
-from json_module import InputFile, InputDir
-from json_module import JsonModule,ModuleParameters
-import marshmallow as mm
+import argschema
 import renderapi
 
-class RenderTransferParameters(ModuleParameters):
-    render_source = mm.fields.Nested(RenderClientParameters)
-    render_target = mm.fields.Nested(RenderClientParameters)
 
-class RenderTransfer(JsonModule):
+class RenderTransferParameters(argschema.schemas.ArgSchema):
+    render_source = argschema.fields.Nested(RenderClientParameters)
+    render_target = argschema.fields.Nested(RenderClientParameters)
+
+class RenderTransfer(argschema.ArgSchemaParser):
     def __init__(self,schema_type=None,*args,**kwargs):
         if schema_type is None:
             schema_type = RenderTransferParameters
         super(RenderTransfer,self).__init__(schema_type = schema_type,*args,**kwargs)
         self.render_source = renderapi.render.connect(**self.args['render_source'])
-        self.render_target = renderapi.render.connect(**self.args['render_target']) 
+        self.render_target = renderapi.render.connect(**self.args['render_target'])
