0691d6c6c500ff978d987e05a900f980025ad917
Forrest Collman <forrestc@alleninstitute.org>
2017-08-19 14:40:18 -0700

    fixed bugs

--- renderapps/TrakEM2/transform_local_annotation_json_to_global.py
+++ renderapps/TrakEM2/transform_local_annotation_json_to_global.py
@@ -54,8 +54,8 @@ def transform_annotations(render,stack,local_annotation):
     #loop over annotations
     for area_list in local_annotation['area_lists']:
         for area in area_list['areas']:
-            ts = next(ts for ts in tilespecs if ts.tileId == tile_path['tileId'])
-            area['global_path']=renderapi.transform.estimate_dstpts(ts.tforms,tile_path['local_path'])
+            ts = next(ts for ts in tilespecs if ts.tileId == area['tileId'])
+            area['global_path']=renderapi.transform.estimate_dstpts(ts.tforms,area['local_path'])
                     
     return local_annotation
 
