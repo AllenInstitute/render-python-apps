98f015b205835bc7c70515e8682b712686edeb5d
Eric Perlman <perlman@gmail.com>
2017-12-07 17:57:50 -0800

    Updated code for moving stacks to the cloud.

--- renderapps/transfer/move_stack.py
+++ renderapps/transfer/move_stack.py
@@ -1,99 +1,133 @@
 import tempfile
 import os
+import sets
 import renderapi
 from .transfer_module import RenderTransferParameters, RenderTransfer
 from argschema.fields import Str, Boolean
 
 
-example_parameters={
-    "source_render":{
-        'host':'ibs-forrestc-ux1',
-        'port':80,
-        'owner':'Forrest',
-        'project':'M247514_Rorb_1',
-        'client_scripts': '/var/www/render/render-ws-java-client/src/main/scripts'
+EXAMPLE_PARAMETERS = {
+    "render_source":{
+        "host":"ibs-forrestc-ux1",
+        "port":80,
+        "owner":"Forrest",
+        "project":"M247514_Rorb_1",
+        "client_scripts": "/var/www/render/render-ws-java-client/src/main/scripts"
     },
-    "target_render":{
-        'host':'ec2-54-202-181-130.us-west-2.compute.amazonaws.com',
-        'port':8080,
-        'owner':'Forrest',
-        'project':'M247514_Rorb_1',
-        'client_scripts': '/var/www/render/render-ws-java-client/src/main/scripts'
+    "render_target":{
+        "host":"ec2-54-202-181-130.us-west-2.compute.amazonaws.com",
+        "port":8080,
+        "owner":"Forrest",
+        "project":"M247514_Rorb_1",
+        "client_scripts": "/var/www/render/render-ws-java-client/src/main/scripts"
     },
-    "stack_in" : 'ALIGNMBP_deconv',
-    "stack_out" : 'ALIGNMBP_deconv',
-    "data_description": "s3:synaptome-playpen:",
-    "replace_chars": "false",
+    "stack_in" : "ALIGNMBP_deconv",
+    "stack_out" : "ALIGNMBP_deconv",
+    "data_destination": "s3://synaptome-playpen",
     "remove_masks": "false",
-    "upload_json": "true"
+    "upload_json": "false",
+    "rewrite_urls": "true"
 }
-class StackTransferParameters(RenderTransferParameters):
-    stack_in = Str(required=True,
-        description='stack to move from source_render')
-    stack_out = Str(required=False,
-        description='stack to move to target_render (default to the same)')
-    data_description = Str(required=False,default='file:',
+
+class StackTransferParametersBase(RenderTransferParameters):
+    data_destination = Str(required=False,default='file:',
         description='replace the ^.*: with this in the image url (default file:)')
-    replace_chars = Boolean(required=False,default=True,
-        description='replace perc20 charactesr with spaces (default True)')
     remove_masks = Boolean(required=False,default=False,
         description='remove the masks (default False)')
     upload_json = Boolean(required=False,default=True,
         description='actually do the upload to the adjacent render,\
          false will still return list of to/from image destinations (default True)')
+    rewrite_urls = Boolean(required=False,default=True, description='rewrite URLs; required for data upload (default true)')
 
+class StackTransferParameters(StackTransferParametersBase):
+    stack_in = Str(required=True,
+        description='stack to move from source_render')
+    stack_out = Str(required=False,
+        description='stack to move to target_render (default to the same)')
 
 class StackTransfer(RenderTransfer):
-    def __init__(self,schema_type=None,*args,**kwargs):
+    def __init__(self, schema_type=None, *args, **kwargs):
         if schema_type is None:
             schema_type = StackTransferParameters
         super(StackTransfer,self).__init__(schema_type=schema_type,*args,**kwargs)
+    
+        self.tfiles = []
+        self.datalist = []
+        self.seen_urls = sets.Set()
+
+    def sanitizeURL(self, url):
+        """Normalize urls into a clean file:/path/form.jpg url."""
+        if url is None or url == "":
+            return None
+        if url[0] == "/":
+            url = "file:" + url
+        # file:/// are also used (and valid)
+        url = url.replace('file:///', 'file:/')
+        # Clean up double slashes
+        while "//" in url:
+            url = url.replace('//', '/')
+        # Use proper spaces
+        url = url.replace("%20", " ")
+        return url
+
+    def transformURL(self, url):
+        """Calculate new path and update worklists."""
+        if self.args['rewrite_urls']:
+            url = self.sanitizeURL(url)
+            (precolon, postcolon) = url.split(':')
+            newurl = self.args['data_destination'] + postcolon
+        else:
+            newurl = url
+
+        if newurl not in self.seen_urls:
+            self.datalist.append((url, newurl))
+            self.seen_urls.add(newurl)
+
+        # Replace spaces with %20 -- may be needed for ImageJ url.open?
+        newurl.replace(" ", "%20")
+        return newurl
+
+    def transform_ip(self, ip):
+        """Find and adjust all of the URLs associated with an ImageProcessor"""
+        for level in ip.values():
+            level.imageUrl = self.transformURL(level.imageUrl)
+            if self.args['remove_masks']:
+                level.maskUrl = None
+            else:
+                level.maskUrl = self.transformURL(level.maskUrl)
+
     def run(self):
         stack_out = self.args.get('stack_out',self.args['stack_in'])
         stack_in =self.args['stack_in']
 
         zvalues = renderapi.stack.get_z_values_for_stack(stack_in, render=self.render_source)
 
-        tfiles = []
-
-        datalist = []
-
         for z in zvalues:
             tilespecs = renderapi.tilespec.get_tile_specs_from_z(
-                stack_in,
-                z,
-                render=self.render_source)
+                stack_in, z, render=self.render_source)
             for ts in tilespecs:
-                url = ts.ip.mipMapLevels[0].imageUrl
-                (precolon, postcolon) = url.split(':')
-                newurl = self.args['data_description'] + postcolon
-
-                ts.ip.mipMapLevels[0].imageUrl = newurl
-                if self.args['replace_chars']:
-                    url = url.replace("%20", " ")
-                    newurl = newurl.replace("%20", " ")
-                datalist.append((url, newurl))
-
-                # TODO: A seperate pass for masks?
-                if self.args['remove_masks']:
-                    ts.ip.mipMapLevels[0].maskUrl=None
+                self.transform_ip(ts.ip)
+                if ts.channels is not None:
+                    for chan in ts.channels:
+                        self.transform_ip(chan.ip)                          
             if self.args['upload_json']:
                 tid,tfile = tempfile.mkstemp(suffix='.json')
                 self.logger.debug(tfile)
                 fid = open(tfile,'w')
                 renderapi.utils.renderdump(tilespecs,fid)
                 fid.close()
-                tfiles.append(tfile)
+                self.tfiles.append(tfile)
+
         if self.args['upload_json']:
-            self.render_target.run(renderapi.stack.create_stack,stack_out)
-            self.render_target.run(renderapi.client.import_jsonfiles_parallel,stack_out,tfiles)
-            self.render_target.run(renderapi.stack.set_stack_state,stack_out,'COMPLETE')
-                    # Clean up temp JSON files
-            for filename in tfiles:
+            self.render_target.run(renderapi.stack.create_stack, stack_out)
+            self.render_target.run(renderapi.client.import_jsonfiles, stack_out, self.tfiles)
+            self.render_target.run(renderapi.stack.set_stack_state, stack_out, 'COMPLETE')
+            # Clean up temp JSON files
+            for filename in self.tfiles:
                 self.logger.debug("Removing " + filename)
                 os.remove(filename)
-        return datalist
+        return self.datalist
 
 if __name__ == "__main__":
-    mod = StackTransfer(input_data= example_parameters)
+    mod = StackTransfer(input_data=EXAMPLE_PARAMETERS)
     mod.run()
--- renderapps/transfer/move_stacks_and_data_to_s3.py
+++ renderapps/transfer/move_stacks_and_data_to_s3.py
@@ -4,11 +4,11 @@ import re
 import boto3
 import botocore
 
+from argschema.fields import Str, Bool, List
 from .transfer_module import RenderTransfer, RenderTransferParameters
-from .move_stack import StackTransfer
-from argschema.fields import Str
+from .move_stack import StackTransfer, StackTransferParametersBase
 
-example_parameters={
+EXAMPLE_PARAMETERS = {
     "source_render":{
         'host':'ibs-forrestc-ux1',
         'port':80,
@@ -23,51 +23,57 @@ example_parameters={
         'project':'M247514_Rorb_1',
         'client_scripts': '/var/www/render/render-ws-java-client/src/main/scripts'
     },
-    "stack_in" : 'ALIGNMBP_deconv',
-    "stack_out" : 'ALIGNMBP_deconv',
-    "data_description": "s3:synaptome-playpen:",
+    "input_stacks" : ["ALIGNMBP_deconv"],
+    "output_stacks" : ["ALIGNMBP_deconv"],
+    "data_destination": "s3://synaptome-playpen:",
     "upload_json": "true",
     "upload_data": "true",
-    "check_only": "false"
+    "check_only": "false",
+    "aws_region": "us-east-1"
 }
 
+class MoveStacksAndDataToS3Parameters(StackTransferParametersBase):
+    check_only = Bool(required=False, default=False, description="Check for file existence but do not upload")
+    upload_data = Bool(required=False, default=False, description="Upload data to S3")
+    input_stacks = List(Str, required=True, description="list of stacks to copy")
+    output_prefix = List(Str, required=False, description="list of names for uploaded stacks (defaults to input_stack)")
+    aws_region = Str(required=False, default=None, allow_none=True, description="AWS region (required for newer regions")
 
-class MoveStackAndDataToS3Parameters(RenderTransferParameters):
-    example = Str(required=True,
-        description='an example')
-    default_val = Str(required=False,default="a default value",
-        description='an example with a default')
-
-
-
-class MoveStackAndDataToS3(RenderTransfer):
-    def __init__(self,schema_type=None,*args,**kwargs):
+class MoveStacksAndDataToS3(RenderTransfer):
+    def __init__(self, schema_type=None, *args, **kwargs):
         if schema_type is None:
-            schema_type = MoveStackAndDataToS3Parameters
-        super(MoveStackAndDataToS3,self).__init__(schema_type=schema_type,*args,**kwargs)
+            schema_type = MoveStacksAndDataToS3Parameters
+        super(MoveStacksAndDataToS3, self).__init__(schema_type=schema_type, *args, **kwargs)
+
     def run(self):
-        print self.args
-        self.logger.error('WARNING NEEDS TO BE TESTED, TALK TO FORREST IF BROKEN')
+        input_stacks = self.args['input_stacks']
+        output_stacks = self.args.get('output_stacks', input_stacks)
 
-        stack_move_params = dict(self.args)
-        stack_move_params.pop('upload_data')
-        stack_move_params.pop('check_only')
-        stack_move_params['replace_chars']=True
-        stack_move_params['remove_masks']=True
-        submod = StackTransfer(input_data = stack_move_params,args=[])
-        datalist=submod.run()
+        for input_stack, output_stack in zip(input_stacks, output_stacks):
+            stack_move_params = dict(self.args)
+            stack_move_params['stack_in']=input_stack
+            stack_move_params['stack_out']=output_stack
+            stack_move_params.pop('upload_data')
+            stack_move_params.pop('check_only')
+            stack_move_params.pop('input_stacks')
+            stack_move_params.pop('output_stacks', None)
+            stack_move_params.pop('input_json', None)
+            stack_move_params.pop('aws_region', None)
 
-        if self.args['upload_data']:
-            self.upload_to_s3(datalist)
+            submod = StackTransfer(input_data=stack_move_params, args=[])
+            datalist = submod.run()
+
+            if self.args['upload_data']:
+                self.upload_to_s3(datalist)
 
     def upload_to_s3(self,datalist):
-        s3 = boto3.resource('s3')
-        bucket_name = re.match(r's3://([^/]*).*', self.args['data_description']).group(1)
-        s3bucket = s3.Bucket(bucket_name)
+        s3conn = boto3.resource('s3', self.args['aws_region'])
+        bucket_name = re.match(r's3://([^/]*).*', self.args['data_destination']).group(1)
+        s3bucket = s3conn.Bucket(bucket_name)
         try:
-            s3.meta.client.head_bucket(Bucket=bucket_name)
+            s3conn.meta.client.head_bucket(Bucket=bucket_name)
         except botocore.exceptions.ClientError as e:
-            self.logger.error("Could not access bucket %s, giving up." % bucket_name)
+            self.logger.error("Could not access bucket %s (%s), giving up.", bucket_name, e)
             os.sys.exit(1)
 
         for (src, dest) in datalist:
@@ -83,13 +89,13 @@ class MoveStackAndDataToS3(RenderTransfer):
                 s3keyexists = False
 
             if s3keyexists:
-                self.logger.info("Found %s, skipping." % dest)
+                self.logger.info("Found %s, skipping.", dest)
             else:
-                self.logger.debug("Upload %s to %s" % (src, dest))
+                self.logger.debug("Upload %s to %s", src, dest)
                 if not self.args['check_only']:
                     data = open(src, 'rb')
                     s3bucket.put_object(Key=s3key, Body=data)
 
 if __name__ == "__main__":
-    mod = MoveStackAndDataToS3(input_data= example_parameters)
+    mod = MoveStacksAndDataToS3(input_data=EXAMPLE_PARAMETERS)
     mod.run()
